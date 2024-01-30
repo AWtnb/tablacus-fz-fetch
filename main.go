72d495d1006add0c344bfd625a09b00fae861868
awtnb <arikaebanataw@gmail.com>
2024-01-30 18:19:19 +0900

    re-structuring

--- main.go
+++ main.go
@@ -43,6 +43,13 @@ func askUser(prompt string) string {
 	return scanner.Text()
 }
 
+func removeFile(path string) error {
+	if err := os.Remove(path); err != nil {
+		return err
+	}
+	return nil
+}
+
 type Dir struct {
 	path string
 }
@@ -79,6 +86,75 @@ func (d Dir) selectFiles() (ps []string, err error) {
 	return
 }
 
+func (d Dir) showLeftFiles() {
+	left := d.getChildren()
+	fmt.Printf("\n[FINISHED] ")
+	if len(left) < 1 {
+		fmt.Printf("No files left on '%s'.\n", d)
+	} else {
+		fmt.Printf("Left file(s) on '%s':\n", d)
+		for _, p := range left {
+			fmt.Printf("- '%s'\n", filepath.Base(p))
+		}
+	}
+	fmt.Scanln()
+}
+
+type SelectedFile struct {
+	path string
+}
+
+func (sf SelectedFile) name() string {
+	return filepath.Base(sf.path)
+}
+
+func (sf SelectedFile) existsOn(dirPath string) bool {
+	p := filepath.Join(dirPath, filepath.Base(sf.path))
+	_, err := os.Stat(p)
+	return err == nil
+}
+
+func (sf SelectedFile) copyTo(dest string) error {
+	srcFile, err := os.Open(sf.path)
+	if err != nil {
+		return err
+	}
+	defer srcFile.Close()
+	newPath := filepath.Join(dest, filepath.Base(sf.path))
+	newFile, err := os.Create(newPath)
+	if err != nil {
+		return err
+	}
+	defer newFile.Close()
+	if _, err = io.Copy(newFile, srcFile); err != nil {
+		return err
+	}
+	return nil
+}
+
+type SelectedFiles struct {
+	paths []string
+}
+
+func (sfs SelectedFiles) copyFiles(dest string) (result []string, err error) {
+	for _, path := range sfs.paths {
+		sf := SelectedFile{path: path}
+		if sf.existsOn(dest) {
+			msg := fmt.Sprintf("Name duplicated: '%s'\noverwrite?", sf.name())
+			ans := askUser(msg)
+			if strings.ToLower(ans) != "y" {
+				fmt.Println("==> skipped")
+				continue
+			}
+		}
+		if err = sf.copyTo(dest); err != nil {
+			return
+		}
+		result = append(result, path)
+	}
+	return
+}
+
 func run(src string, dest string) int {
 	if src == dest {
 		return 1
@@ -102,24 +178,12 @@ func run(src string, dest string) int {
 		}
 		return 1
 	}
-	var copied []string
-	for _, path := range selected {
-		n := filepath.Base(path)
-		newPath := filepath.Join(dest, n)
-		if isValidPath(newPath) {
-			msg := fmt.Sprintf("Name duplicated: '%s'\noverwrite?", n)
-			ans := askUser(msg)
-			if strings.ToLower(ans) != "y" {
-				fmt.Println("==> skipped")
-				continue
-			}
-		}
-		err := copyFile(path, newPath)
-		if err != nil {
-			report(err.Error())
-			continue
-		}
-		copied = append(copied, path)
+
+	sfs := SelectedFiles{paths: selected}
+	copied, err := sfs.copyFiles(dest)
+	if err != nil {
+		report(err.Error())
+		return 1
 	}
 	if len(copied) < 1 {
 		return 0
@@ -127,81 +191,8 @@ func run(src string, dest string) int {
 	if err := askRemoveFiles(copied); err != nil {
 		report(err.Error())
 	}
-	showLeftFiles(src)
+	d.showLeftFiles()
 	return 0
-}
-
-func isValidPath(path string) bool {
-	_, err := os.Stat(path)
-	return err == nil
-}
-
-func getChildren(d string) []string {
-	var ps []string
-	fs, err := os.ReadDir(d)
-	if err != nil {
-		return ps
-	}
-	for _, f := range fs {
-		if strings.HasSuffix(f.Name(), ".ini") || strings.HasPrefix(f.Name(), "~$") || f.IsDir() {
-			continue
-		}
-		p := filepath.Join(d, f.Name())
-		ps = append(ps, p)
-	}
-	return ps
-}
-
-func showLeftFiles(d string) {
-	left := getChildren(d)
-	fmt.Printf("\n[FINISHED] ")
-	if len(left) < 1 {
-		fmt.Printf("No files left on '%s'.\n", d)
-	} else {
-		fmt.Printf("Left file(s) on '%s':\n", d)
-		for _, p := range left {
-			fmt.Printf("- '%s'\n", filepath.Base(p))
-		}
-	}
-	fmt.Scanln()
-}
-
-func selectFilePaths(paths []string) ([]string, error) {
-	var ps []string
-	idxs, err := fuzzyfinder.FindMulti(paths, func(i int) string {
-		return filepath.Base(paths[i])
-	}, fuzzyfinder.WithCursorPosition(fuzzyfinder.CursorPositionTop))
-	if err != nil {
-		return ps, err
-	}
-	for _, i := range idxs {
-		ps = append(ps, paths[i])
-	}
-	return ps, err
-}
-
-func copyFile(src string, newPath string) error {
-	srcFile, err := os.Open(src)
-	if err != nil {
-		return err
-	}
-	defer srcFile.Close()
-	newFile, err := os.Create(newPath)
-	if err != nil {
-		return err
-	}
-	defer newFile.Close()
-	if _, err = io.Copy(newFile, srcFile); err != nil {
-		return err
-	}
-	return nil
-}
-
-func removeFile(path string) error {
-	if err := os.Remove(path); err != nil {
-		return err
-	}
-	return nil
 }
 
 func askRemoveFiles(paths []string) error {
